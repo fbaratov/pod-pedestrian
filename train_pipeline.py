cd3e99ef28cf84bc157632ef326345b58d97f046
Farrukh Baratov <farrukhsbaratov@gmail.com>
2022-04-28 02:34:53 +0200

    images are successfully processed and visualized

--- train_pipeline.py
+++ train_pipeline.py
@@ -1,4 +1,5 @@
 import os
+from random import random, sample
 
 import numpy as np
 from paz.abstract import ProcessingSequence, SequentialProcessor
@@ -6,6 +7,8 @@ from paz.models.detection.utils import create_prior_boxes
 from paz.pipelines import AugmentDetection
 import paz.processors as pr
 import paz.backend as P
+
+from caltech_processor import AugmentCaltech
 
 
 def deprocess_image(image):
@@ -40,11 +43,18 @@ def extract_box_caltech(file, width=640, height=480):
 
             split = line.split(" ")
             label = split[0]
-            x0, y0, width, height = [int(val) for val in split[1:5]]
-            x1 = x0 + width
-            y1 = y0 + height
+            x0, y0, bw, bh = [int(val) for val in split[1:5]]
+            x1 = x0 + bw
+            y1 = y0 + bh
             label_int = model_labels[label]
-            box_data.append([x0, y0, x1, y1, label_int])
+            if label_int != 1:  # skip crowds and invalid numbers
+                continue
+            elif np.nan in (x0, y0, x1, y1) or np.inf in (x0, y0, x1, y1):
+                continue
+            elif not (x0 < x1 and y0 < y1):
+                continue
+
+            box_data.append([x0 / width, y0 / height, x1 / width, y1 / height, label_int])
 
     return np.array(box_data)
 
@@ -63,11 +73,11 @@ def prep_data():
         print(set)
         for vdir in os.listdir(f"{annot_dir}/{set}"):
             print(f"  {vdir}")
-            for annot_name in os.listdir(f"{annot_dir}/{set}/{vdir}"):
-                num = annot_name[1:-4]
+            for annot_file in os.listdir(f"{annot_dir}/{set}/{vdir}"):
+                num = annot_file[1:-4]
                 img_name = f"{set}_{vdir}_0{num}.jpg"  # get image name (will have to change once image names are fixed)
                 img_path = f"{img_dir}/{set}/{img_name}"  # assemble image path
-                annot_path = f"{annot_dir}/{set}/{vdir}/{annot_name}"  # assemble annotation path
+                annot_path = f"{annot_dir}/{set}/{vdir}/{annot_file}"  # assemble annotation path
 
                 # extract boxes
                 boxes = extract_box_caltech(annot_path)
@@ -80,17 +90,17 @@ def prep_data():
                     "image": img_path,
                     "boxes": boxes
                 })
+                #if data:
+                    #break
         break
-    print(data[0:10])
     return data
 
 
 def caltech():
     data = prep_data()
 
-    class_names = ['background', 'person', 'people']
-    prior_boxes = create_prior_boxes()
-    augmentator = AugmentDetection(prior_boxes, num_classes=len(class_names))
+    class_names = ['person', 'people']
+    augmentator = AugmentCaltech(num_classes=len(class_names))
 
     draw_boxes = SequentialProcessor([
         pr.ControlMap(pr.ToBoxes2D(class_names, True), [1], [1]),
@@ -99,18 +109,25 @@ def caltech():
         pr.ShowImage()])
 
     print('Image and boxes augmentations with generator')
-    batch_size = 1
+    batch_size = 5
     sequence = ProcessingSequence(augmentator, batch_size, data)
-    while True:
+    for i in sample(range(0, 1000), 5):
         try:
-            batch = sequence.__getitem__(0)
+            batch = sequence.__getitem__(i)
         except:
+            print("continue")
             continue
         batch_images, batch_boxes = batch[0]['image'], batch[1]['boxes']
         image, boxes = batch_images[0], batch_boxes[0]
         image = deprocess_image(image)
-        draw_boxes(image, boxes)
-        break
+        print(boxes[0])
+        for i,b in enumerate(boxes):
+            if b[0] >= b[2] or b[1] >= b[3]:
+                np.delete(boxes, i)
+        #draw_boxes(image, boxes)
 
-if __name__=="__main__":
+    return
+
+
+if __name__ == "__main__":
     caltech()
